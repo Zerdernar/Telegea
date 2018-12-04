db8dfb27c440824b196282887d58a96c00d0cf95
schooluser <malboi@mail.ru>
2018-12-04 15:02:44 +0500

    qwert

--- app/src/main/java/com/example/schooluser/telegea/LeftMenuFragment.kt
+++ app/src/main/java/com/example/schooluser/telegea/LeftMenuFragment.kt
@@ -5,9 +5,17 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_left_menu.*
 
 class LeftMenuFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_left_menu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sw1.setOnClickListener {
+
+        }
     }
 }
